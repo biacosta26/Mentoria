6ff0d8105c8aaf3498acc661118d305f97a222a1
biacosta26 <ana.araujo.42@aluno.edu.pi.gov.br>
2025-05-30 13:26:22 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,1 +1,10 @@
-import
+import java.util.Scanner//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class temperatura {
+Scanner entrada=new Scanner(System);
+System.out.println("digite a temperatura em celsius");
+Double celsius=entrada.nextDouble();
+Double fahrenheit=(celsius *9/5)+32;
+System.out.println("a temperatura em fahreinheit é:"+fahrenheit);
+ entrada.clase():
+}
